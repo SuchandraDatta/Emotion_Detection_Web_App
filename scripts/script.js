7489992a6fcfd1eaaa75fb28ea26fc7e11bfc5f8
SuchandraDatta <41965125+SuchandraDatta@users.noreply.github.com>
2021-06-19 16:21:58 +0530

    Minor changes

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,14 @@ URL_DISGUST = "https://animechan.vercel.app/api/quotes/anime?title=Death Note"
 URL_FEAR = "https://animechan.vercel.app/api/quotes/anime?title=Shirobako"
 URL_SURPRISE = "https://animechan.vercel.app/api/quotes/anime?title=Dragon Ball Z"
 URL_LIST = [URL_ANGRY, URL_DISGUST, URL_FEAR, URL_HAPPY, URL_SAD, URL_SURPRISE, URL_NEUTRAL]
-
+//This array is encoded with index i = corresponding emotion. In dataset, 0 = Angry, 1 = Disgust, 2 = Fear, 3 = Happy, 4 = Sad, 5 = Surprise and 6 = Neutral
+		emotions = ["Angry", "Disgust", "Fear", "Happy", "Sad", "Surprise", "Neutral"]
+model = null
+async function load_model()
+{
+	model = await tf.loadLayersModel('model.json');
+}
+model = load_model()//Load only once per page load
 
 function display(event)
 	{
@@ -18,9 +25,11 @@ function display(event)
 async function predict_emotion()
 	{
 		pleasewait = document.getElementById("pleasewait").style.display="flex";
-		output = document.getElementById("output_chart")
-		    	output.innerHTML = ""
-		    	
+		window.scrollBy(0, 100)
+		output_screen = document.getElementsByClassName("output_screen")[0]
+		output_screen.style.display = "None"
+		quotes_screen = document.getElementsByClassName("quotes_screen")[0]
+		quotes_screen.style.display = "None"		   
 		let input = document.getElementById("input_image");
 		//Preprocessing steps 
 		/*
@@ -30,13 +39,17 @@ async function predict_emotion()
 		(4)Reshape to (1,48,48,1)
 		(5)Normalize by dividing by 255.0
 		*/
-		let step1 = tf.browser.fromPixels(input).resizeNearestNeighbor([48,48]).mean(2).toFloat().expandDims(0).expandDims(-1).div(255.0)
-		const model = await tf.loadLayersModel('model.json');
-		pred = model.predict(step1)
-		pred.print()
-		console.log("End of predict function")
-		//This array is encoded with index i = corresponding emotion. In dataset, 0 = Angry, 1 = Disgust, 2 = Fear, 3 = Happy, 4 = Sad, 5 = Surprise and 6 = Neutral
-		emotions = ["Angry", "Disgust", "Fear", "Happy", "Sad", "Surprise", "Neutral"]
+		try
+		{
+			let step1 = tf.browser.fromPixels(input).resizeNearestNeighbor([48,48]).toFloat().mean(2).expandDims(0).expandDims(-1).div(255.0)						
+			pred = model.predict(step1)
+			pred.print()
+		}
+		catch(err)
+		{
+			document.getElementById("pleasewait").innerHTML="Error occurred please try again later"
+		}
+		
 		//At which index in tensor we get the largest value ?
 		pred.data()
 		    .then((data) => {console.log(data)
@@ -59,29 +72,26 @@ async function predict_emotion()
 				document.getElementsByClassName("output_screen")[0].style.display="flex";
 				document.getElementById("output_text").innerHTML=""
 				document.getElementById("output_text").innerHTML = "<p>Emotions and corresponding scaled up probability</p><p>Emotion detected: " + EMOTION_DETECTED + "(" + (max_val*100).toFixed(2) + "% probability)</p>"
+				window.scrollBy(0, 100)
 				//Now display quotes depending on detected emotion
 				fetch(URL_LIST[max_val_index])
 				.then(response => response.json())
 				.then((data) =>{
-						console.log(data)
 				innerHTMLtext = ""
 				for(let i=0;i<data.length;i++)
 				{
-					console.log(data[i]["quote"])
-					console.log(data[i]["character"])
 					if(data[i]["quote"].length<=200)
 					{
 						innerHTMLtext+="<div class='quotes_pane'><div class='quote'>"+data[i]["quote"]+"</div></div>"
 					}
 				}
-				document.getElementsByClassName("quotes_screen")[0].innerHTML = innerHTMLtext
+				quotes_screen = document.getElementsByClassName("quotes_screen")[0]
+				quotes_screen.innerHTML = innerHTMLtext
+				quotes_screen.style.display = "flex"
+				window.scrollBy(0, 100)
+
 			}).catch(()=>{document.getElementById("pleasewait").innerHTML="Error occurred please try again later"})
-		}).catch(()=>{document.getElementById("pleasewait").innerHTML="Error occurred please try again later"})
-		/*required_index = pred.argMax(axis=1)
-		required_index.print()
-		required_index.data().then((data) => {
-			const EMOTION_PREDICTED = emotions[data["0"]]
-			console.log("The predicted emotion is " + EMOTION_PREDICTED)
-		})*/
+		}).catch((err)=>{document.getElementById("pleasewait").innerHTML="Error occurred please try again later"})
+	pred.dispose()
 
 	}
